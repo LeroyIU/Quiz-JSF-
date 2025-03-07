c0308d53ce762c212041a5d819769e2afce1bbc2
Michael Landinger <michael.landinger@iu-study.org>
2025-03-07 20:49:48 +0100

    Work on Darkdmode/Lightmode

---
+++ src/main/java/ThemeSwitcherBean.java
@@ -0,0 +1,22 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class ThemeSwitcherBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private boolean darkTheme = false; // Default to light theme
+
+    public boolean isDarkTheme() {
+        return darkTheme;
+    }
+
+    public void setDarkTheme(boolean darkTheme) {
+        this.darkTheme = darkTheme;
+    }
+
+    public String getTheme() {
+        return darkTheme ? "arya" : "saga";
+    }
+}
