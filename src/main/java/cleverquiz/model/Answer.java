bda4c708cc4e96ca5edf74feaaba6c4f0e1e41d0
leroy <info@blacklinerdev.de>
2025-03-10 21:32:29 +0100

    Add Answer

--- src/main/java/cleverquiz/model/Answer.java
+++ src/main/java/cleverquiz/model/Answer.java
@@ -2,52 +2,13 @@ package cleverquiz.model;
 
 import jakarta.persistence.*;
 
-/**
- * Entity class representing an answer.
- */
 @Entity
 @Table(name = "Answer")
 public class Answer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer answerId;
 
     private String text;
     private Boolean correctness;
-    @ManyToOne
-    @JoinColumn(name = "question_id", nullable = false)
-    private Question question;
-
-    public Boolean getCorrectness() {
-        return correctness;
-    }
-
-    public void setCorrectness(Boolean correctness) {
-        this.correctness = correctness;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Integer getAnswerId() {
-        return answerId;
-    }
-
-    public void setAnswerId(Integer answerId) {
-        this.answerId = answerId;
-    }
-
-    public Question getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
 }
