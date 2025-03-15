0b74e369b62460b156fdf9aa52b71fe30bcf7cef
leroy <info@blacklinerdev.de>
2025-03-15 07:27:40 +0100

    Add addCategory to db

--- src/main/java/cleverquiz/controller/Controller.java
+++ src/main/java/cleverquiz/controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller implements IController {
 
     @Override
     public boolean addCategory(String name) {
-        return false;
+        return DBUtil.addCategory(name);
     }
 
     @Override
