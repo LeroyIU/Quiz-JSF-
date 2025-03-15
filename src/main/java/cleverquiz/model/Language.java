65095d123e304ad6c08d1f1f6e37baa63bd39658
leroy <info@blacklinerdev.de>
2025-03-15 13:08:59 +0100

    Add equals & hashCode method for Language

--- src/main/java/cleverquiz/model/Language.java
+++ src/main/java/cleverquiz/model/Language.java
@@ -2,6 +2,8 @@ package cleverquiz.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 /**
  * Entity class representing a language.
  */
@@ -21,5 +23,18 @@ public class Language {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Language language = (Language) obj;
+        return Objects.equals(languageId, language.languageId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(languageId);
     }
 }
