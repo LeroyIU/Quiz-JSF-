6a7b6c1d63099e6c1a7fef31a36564fc6ab81557
leroy <info@blacklinerdev.de>
2025-03-21 11:44:48 +0100

    Add title and author to News (Post)

--- src/main/java/cleverquiz/model/News.java
+++ src/main/java/cleverquiz/model/News.java
@@ -14,27 +14,59 @@ public class News {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer postId;
 
+    private String title;
     private String text;
     private Date date;
+    private User author;
 
     public Integer getPostId() {
         return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getText() {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public Date getDate() {
         return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     @Override
     public String toString() {
         return "News{" +
                 "postId=" + postId +
+                ", title='" + title + '\'' +
                 ", text='" + text + '\'' +
                 ", date=" + date +
+                ", author=" + author +
                 '}';
     }
 }
